8c7698898fe0e8f1362501e6dfdacc32eee34429
AfridShaik <shaikrehamaan2003@gmail.com>
2024-01-25 20:58:41 -0800

    Committed

--- Print.java
+++ Print.java
@@ -1,6 +1,7 @@
 public class Print {
     public static void main(String[] args) {
-        System.out.println("Hello"); 
+        System.out.println("Hello");
+        System.out.println("hai"); 
        }
     
 }
